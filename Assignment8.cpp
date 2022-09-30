fe58d3bbe0507efd4267e294d04a81dc27f30a24
Pruthviraj002 <114584082+Pruthviraj002@users.noreply.github.com>
2022-09-30 14:13:54 +0530

    Input side of square and calculate the area

---
+++ Assignment8.cpp
@@ -0,0 +1,14 @@
+/*Write a program to input side of a square and calculate the area.*/
+#include<stdio.h>
+int main()
+{
+	int side,area;
+	
+	printf("\n Enter value of side=");
+	scanf("%d",&side);
+	
+	area=side*side;
+	printf("\n The Area of Square = %d",area);
+	
+	
+}
