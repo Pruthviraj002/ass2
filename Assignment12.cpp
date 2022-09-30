86f032c43e7f8fb7179752b8e5e1663701f47b44
Pruthviraj002 <114584082+Pruthviraj002@users.noreply.github.com>
2022-09-30 14:18:56 +0530

    Add files via upload
    Input number and print it equivalent character code

---
+++ Assignment12.cpp
@@ -0,0 +1,15 @@
+/*Write a program to input ASCII value and display its equivalent character*/
+#include<stdio.h>
+int main()
+{
+   int AV;
+   
+   printf("\n Enter value of ASCII value :");
+   scanf("%d",&AV);
+   
+   printf("\n ASCII value= %d",AV);
+   
+   printf("\n Character = %c",AV);	
+}
+
+
