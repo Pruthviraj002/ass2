b164c8fc94907929c7c2ac869b891c748b37738d
Pruthviraj002 <114584082+Pruthviraj002@users.noreply.github.com>
2022-09-30 14:22:04 +0530

    Add files via upload
    Input to number and print the greatest using conditional operator

---
+++ Assignment15.cpp
@@ -0,0 +1,11 @@
+/*Write a program to input two numbersand print the greatest using conditional 
+operator.*/
+#include<stdio.h>
+int main()
+{
+	int a=99;
+	int b=100;
+	
+	a>b ? printf("\n a is max.") : printf("\n b is max.");
+		
+}
