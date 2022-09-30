c72bb696564496e44709ab7cfcb487cf6a55127f
Pruthviraj002 <114584082+Pruthviraj002@users.noreply.github.com>
2022-09-30 14:03:14 +0530

    Ass3
    Find out square of given number

---
+++ Assignment3.cpp
@@ -0,0 +1,13 @@
+/*Write a program to find out square of a given number.*/
+#include<stdio.h>
+int main()
+{
+	int num,square;
+	
+		
+	printf("\n Enter Any Integer :");
+	scanf("%d",&num);
+	
+	square=num*num;
+	printf("\n Square of %d is = %d",num,square);
+}
