5e111e7add523102a2d65a84d1d5bb6866c6de7a
Pruthviraj002 <114584082+Pruthviraj002@users.noreply.github.com>
2022-09-30 14:18:02 +0530

    Assignment 11
    Print ASCII value

---
+++ Assignment11.cpp
@@ -0,0 +1,14 @@
+/*Write a program to input any character from user and Display its ASCII value*/
+#include<stdio.h>
+int main()
+{
+	char ch;
+	
+	printf("\n Enter value of ch :");
+	scanf("%c",&ch);
+	
+	printf("\n Character = %c",ch);
+	
+	printf("\n ASCII value of ch = %d",ch);
+}
+
