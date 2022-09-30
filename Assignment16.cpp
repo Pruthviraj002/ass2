4e95e0ab37cac83b3ec7f213057df600b9bf504b
Pruthviraj002 <114584082+Pruthviraj002@users.noreply.github.com>
2022-09-30 14:09:42 +0530

    Calculate area of perimeter

---
+++ Assignment16.cpp
@@ -0,0 +1,9 @@
+/*Write a program to input marks of a student and print the result (pass/fail) using 
+conditional operator.*/
+#include<stdio.h>
+int main()
+{
+	int marks=36;
+	
+	marks>35 ? printf("\n Pass") :printf("\n Fail");
+}
